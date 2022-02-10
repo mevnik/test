256973b3436d7530f83f043d06b344d317d023ef
mevnik <mevnik@gmail.com>
2022-02-10 18:26:25 +0100

    add webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+var Ah = function () {
+	console.log("App: init");
+};
+
+var ah = new Ah();
+
+
+document.querySelector('button').addEventListener('click', function(event) {
+	alert ('yes');
+
+    console.log('small sqr');
+})
